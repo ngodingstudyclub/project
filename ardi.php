4bca64655661e2c63125f0ac7849b6809a3d04ce
ardigunawan-it <ardigunawantkj@gmail.com>
2020-03-22 20:11:24 +0700

    terkirim

---
+++ ardi.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="nsc/style/res.css" type="text/css" />
+<title>Document</title>
+</head>
+<body>
+<div class="header">
+<h1>Ngoding Study Club</h1>
+<p>Merajut Mimpi untuk Mewujudkan Mahasiswa Berprestasi</p>
+</div>
+<div class="topnav">
+<a href="index.html">Home</a>
+<a href="layout.html">Layout</a>
+<a href="form.html">Form</a>
+<a href="table.html">Tabel</a>
+<a href="#" style="float: right">Logout</a>
+</div>
